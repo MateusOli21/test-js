22c0fb1150c60131b72f20501135cbef9b9786b3
Mateus Oliveira <mateus.oli.abreu@hotmail.com>
2023-01-26 10:59:55 -0300

    Create webcomp01.js

---
+++ webcomp01.js
@@ -0,0 +1,31 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+<style>
+    .mp-button-cta {
+        width: fit-content;
+        padding: 8px 12px;
+        border-radius: 8px;
+        background-color: #5386E4;
+    }
+
+    .mp-button-cta p {
+        font-weight: bold;
+        color: #fff;
+    }
+</style>
+
+<div class="mp-button-cta">
+    <p>Compre com Mercado Credits</p>
+</div>
+`;
+
+class MercadoPagoWidget extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+window.customElements.define("mercado-pago-widget", MercadoPagoWidget);
