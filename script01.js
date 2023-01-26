cfa67dfe6e988a10451ccb7915d0d3ffe0ba09dd
Mateus Oliveira <mateus.oli.abreu@hotmail.com>
2023-01-26 10:35:37 -0300

    Update script01.js

--- script01.js
+++ script01.js
@@ -3,14 +3,18 @@ function loadMpWebComponent() {
         <mercado-pago-widget></mercado-pago-widget>
     `;
 
+  console.log("adicionado html widget");
   const widgetScript = document.createElement("script");
   widgetScript.src = "https://cdn.jsdelivr.net/gh/MateusOli21/test-js@main/mp-web-com.js";
   widgetScript.type = "text/script";
   widgetScript.id = "mp-widget-script";
 
+  console.log("adicionado script widget");
   let priceHolderElement = document.getElementsByClassName("price-holder");
   priceHolderElement[0].appendChild(htmlWidget);
   priceHolderElement[0].appendChild(widgetScript);
+  
+  console.log("finalizado script de carregamento");
 }
 
 loadMpWebComponent();
