640da53af9ac0da3b004c94614a2a11ffac76efd
Mateus Oliveira <mateus.oli.abreu@hotmail.com>
2023-01-26 11:00:58 -0300

    Update script04.js

--- script04.js
+++ script04.js
@@ -2,7 +2,7 @@ function loadMpWebComponent() {
     let htmlWidget = `<mercado-pago-widget></mercado-pago-widget>`;
   
     const widgetScript = document.createElement("script");
-    widgetScript.src = "https://cdn.jsdelivr.net/gh/MateusOli21/test-js@main/mp-web-com.js";
+    widgetScript.src = "https://cdn.jsdelivr.net/gh/MateusOli21/test-js@main/webcomp01.js";
     widgetScript.type = "text/script";
     widgetScript.id = "mp-widget-script";
     
