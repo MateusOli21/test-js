a564595f5559e24e8cb415e014ca1dfb711422cf
Mateus Oliveira <mateus.oli.abreu@hotmail.com>
2023-07-05 10:09:04 -0300

    Create script11.js

---
+++ script11.js
@@ -0,0 +1,17 @@
+(function () {
+  // Your JavaScript
+  iframe = document.querySelector("#iFrameResizer0")
+  iframe.contentWindow.postMessage('test-event',  "https://checkout-security.ms.tiendanube.com")
+  
+  // window.document.addEventListener('installments', handleEvent, false)
+  // function handleEvent(e) {
+  //   console.log("EVENTO DO IFRAME", e)
+  // }
+  // window.SDKCheckout.changePaymentBenefit({
+  //   id: 'mercado_pago_checkout_pro',
+  //   value:
+  //     LS.country == 'BR'
+  //       ? `Até ${installmentsWithoutInterest} parcelas sem juros`
+  //       : `Hasta ${installmentsWithoutInterest} meses sin interés`
+  // })
+})();
