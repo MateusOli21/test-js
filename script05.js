f5950074593b7555a4000e2fb811207a50959326
Mateus Oliveira <mateus.oli.abreu@hotmail.com>
2023-07-05 09:09:33 -0300

    Create script05.js

---
+++ script05.js
@@ -0,0 +1,18 @@
+(function () {
+  // Your JavaScript
+  var installmentsWithoutInterest = 12
+  var testInstallments = 12
+
+  const test = document.querySelector('#iFrameResizer0')
+  test.addEventListener("installments", (event) => {
+    testInstallments = event.details.card.supportedPaymentMethods.credit_card.max_installments_without_interest
+  })
+  
+  window.SDKCheckout.changePaymentBenefit({
+    id: 'mercado_pago_checkout_pro',
+    value:
+      LS.country == 'BR'
+        ? `Até ${installmentsWithoutInterest} parcelas sem juros`
+        : `Hasta ${installmentsWithoutInterest} meses sin interés`
+  })
+})();
